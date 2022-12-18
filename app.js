763efc46eab3544d9c4b1503e178471ab5011071
tripparsugo <lagraf@usi.ch>
2022-12-18 18:03:44 +0100

    imporve stat

--- app.js
+++ app.js
@@ -11,19 +11,23 @@ function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function generateSeed() {
+    return [
+        Math.floor(Math.random() * 0x10000),
+        Math.floor(Math.random() * 0x10000),
+        Math.floor(Math.random() * 0x10000),
+        Math.floor(Math.random() * 0x10000),
+    ]
+}
 
 function shuffle(originalArray) {
     let array = [].concat(originalArray);
     let currentIndex = array.length, temporaryValue, randomIndex;
 
-    // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-
-        // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
 
-        // And swap it with the current element.
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
@@ -32,11 +36,125 @@ function shuffle(originalArray) {
     return array;
 }
 
-async function doBattle() {
+function pickIfOnlyOption(activeP, ownTeam, otherActiveP, request) {
+    const activeMoves = activeP.moves;
+    if (activeMoves.length === 1) {
+        return {"type": "attack", "value": (0)}
+    }
+    return null
+}
+
+
+function powerUpIfAtHighHp(){
+    return null
+}
+
+function swapIfNoEffectiveMoves(){
+    return null
+}
+
+function pickRandomMove(activeP, ownTeam, otherActiveP, request) {
+    const activeMoves = activeP.moves;
+    if (activeMoves.length === 1) {
+        return {"type": "attack", "value": (0)}
+    }
+    const activeMovesIdx = shuffle([...Array(activeMoves.length).keys()])
+    for (let i of activeMovesIdx) {
+        let move = activeMoves[i]
+        if (move.pp > 0 && !move.disabled) {
+            return {"type": "attack", "value": (i + 1)}
+        }
+    }
+
+    throw new Error("nm")
+}
+
+
+function pickEffectiveMove(activeP, ownTeam, otherActiveP, request) {
+    const activeMoves = activeP.moves;
+    let bestPower = 0
+    let bestIdx = -1
+    const effToMultiplier = new Map();
+    effToMultiplier.set(-2, 0.25)
+    effToMultiplier.set(-1, 0.5)
+    effToMultiplier.set(0, 1)
+    effToMultiplier.set(1, 2)
+    effToMultiplier.set(2, 4)
+
+    for (let i = 0; i < activeMoves.length; i++) {
+        const activeMove = activeMoves[i]
+        const activeMoveDex = pkm.Dex.getActiveMove(activeMove.id)
+        if (activeMove.pp === 0 || activeMove.disabled) {
+            continue
+        }
+        if (activeMoveDex.category === "status") {
+            continue
+        }
+
+        if (activeMoveDex.basePower === 0){
+            continue
+        }
+
+        if (!pkm.Dex.getImmunity(activeMoveDex.type, otherActiveP[0].baseSpecies.types)) {
+            continue
+        }
+
+        const activeTypes = ownTeam.filter(p=>p.isActive)[0].baseSpecies.types
+        const moveType = activeMoveDex.type
+        const movePower = activeMoveDex.basePower
+        const isStab = activeTypes.includes(moveType)
+        const eff = pkm.Dex.getEffectiveness(moveType, otherActiveP[0].baseSpecies.types)
+        const moveAccuracy = activeMoveDex.accuracy
+        let multiplier = effToMultiplier.get(eff)
+        if(isStab){
+            multiplier = multiplier * 1.5
+        }
+        const actualMovePower = multiplier * movePower
+        const actualAverageMovePower = actualMovePower * moveAccuracy/100
+        if (actualAverageMovePower > bestPower) {
+            bestIdx = i
+            bestPower = actualMovePower
+        }
+    }
+    if (bestIdx > -1) {
+        return {"type": "attack", "value": (bestIdx + 1)}
+    }
+
+    return null
+}
+
+function makeStrategyHandler(strategies, fallbackStrategy, strategyName) {
+    function inF(activeP, ownTeam, otherActiveP, request) {
+        for (let strategy of strategies) {
+            const move = strategy(activeP, ownTeam, otherActiveP, request)
+            if (move) {
+                return move
+            }
+        }
+        return fallbackStrategy(activeP, ownTeam, otherActiveP, request)
+    }
+
+    return {strategyHandler: inF, strategyName}
+
+}
+
+
+async function doBattle(log=false) {
     const team1 = pkm.Teams.generate(format)
     const team2 = pkm.Teams.generate(format)
-    const p1 = {name: "Alice", team: pkm.Teams.pack(team1), id: "p1"}
-    const p2 = {name: "Bob", team: pkm.Teams.pack(team2), id: "p2"}
+    const p1 = {
+        name: "Alice",
+        team: pkm.Teams.pack(team1),
+        id: "p1",
+        strategy: makeStrategyHandler([pickEffectiveMove], pickRandomMove, "best")
+    }
+    const p2 = {
+        name: "Bob",
+        team: pkm.Teams.pack(team2),
+        id: "p2",
+        strategy: makeStrategyHandler([], pickRandomMove, "random")
+    }
+    const players = [p1,p2]
     const stream = new pkm.BattleStream();
     const formatInput = {formatid: "${format.id}"}
     stream.write(`>start ${JSON.stringify(formatInput)}`);
@@ -44,33 +162,43 @@ async function doBattle() {
     stream.write(`>player ${p2.id} ${JSON.stringify(p2)}`);
     // console.log("@@1")
     for await (const output of stream) {
-        if(stream.buf.length === 0){
+        if (stream.buf.length === 0) {
             // console.log("@@4")
             await timeout(10)
             // console.log("@@5")
-            if(stream.buf.length === 0){
+            if (stream.buf.length === 0) {
                 return null
                 // console.log("@@6")
             }
         }
         // console.log("@@2")
-        // console.log(output)
+        if(log) {
+            // console.log(output)
+        }
         for (let tmp of output.split("\n")) {
             if (tmp.startsWith("|error|")) {
                 throw new Error("Did not finish battle")
             }
 
             if (tmp.startsWith("|win|")) {
-                const winnerName = tmp.substring("|win|".length + 1)
-                const [winnerTeam, loserTeam] = winnerName === "Alice" ?
-                    [pkm.Teams.unpack(p1.team), pkm.Teams.unpack(p2.team)] :
-                    [pkm.Teams.unpack(p2.team), pkm.Teams.unpack(p1.team)]
-                const result = [winnerTeam, loserTeam]
+                const winnerName = tmp.substring("|win|".length)
+                const winnerPlayer = players.filter(p => p.name === winnerName)[0]
+                if(log){
+                    console.log(winnerName + " won")
+                }
+                const loserPlayer = players.filter(p => p.name !== winnerName)[0]
+                const winnerTeam = pkm.Teams.unpack(winnerPlayer.team)
+                const loserTeam = pkm.Teams.unpack(loserPlayer.team)
+                const winnerStrategy = winnerPlayer.strategy.strategyName
+                const loserStrategy = loserPlayer.strategy.strategyName
+                const result = [winnerTeam, loserTeam, winnerStrategy, loserStrategy]
                 return result
             }
             if (tmp.startsWith("|request|")) {
                 const request = JSON.parse(tmp.substring("|request".length + 1))
                 const playerId = request.side.id;
+                const player = players.filter( p=>p.id===playerId)[0]
+
                 if (request.wait) {
                     continue
                 }
@@ -79,23 +207,36 @@ async function doBattle() {
                         let p = request.side.pokemon[i]
                         if (!p.active && p.condition !== "0 fnt") {
                             const command = `>${playerId} switch ${i + 1}`
-                            // console.log(command)
+                            if(log) {
+                                console.log(command)
+                            }
                             stream.write(command);
                             break
                         }
                     }
                     continue
                 }
-                const activeMoves = request.active[0].moves;
-                for (let i = 0; i < activeMoves.length; i++) {
-                    let move = activeMoves[i]
-                    if (move.pp > 0 && !move.disabled) {
+                const activeP = request.active[0]
+                const otherActiveP = stream.battle.sides[0].id === playerId ? stream.battle.sides[1].active : stream.battle.sides[0].active
+                const ownTeam = stream.battle.sides[0].id === playerId ? stream.battle.sides[0].pokemon : stream.battle.sides[1].pokemon
+                const {strategyHandler, strategyName} = player.strategy
 
-                        const command = `>${playerId} move ${i + 1}`
-                        // console.log(command)
-                        stream.write(command);
-                        break
+                const move = strategyHandler(activeP, ownTeam, otherActiveP, request)
+
+                if (move.type === "attack") {
+                    const command = `>${playerId} move ${move.value}`
+                    stream.write(command);
+                    if(log) {
+                        const availableMoves = activeP.moves.filter(m=>m.pp>0 && m.disabled === false).map(m=>m.move)
+                        console.log("active pkm: " + request.side.pokemon.filter(p=>p.active)[0].details)
+                        console.log("available moves: " + availableMoves)
+                        console.log("opposing pkmn: " + otherActiveP)
+                        console.log("picked move: " + availableMoves[move.value -1])
+
+                        console.log(command)
                     }
+                } else {
+                    throw new Error("Unknown action")
                 }
             }
         }
@@ -104,6 +245,7 @@ async function doBattle() {
 
     throw new Error("Did not finish battle")
 }
+
 
 function convertToCSV(arr) {
     const array = [Object.keys(arr[0])].concat(arr)
@@ -114,7 +256,7 @@ function convertToCSV(arr) {
 }
 
 
-function toTeamData(team, won) {
+function toTeamData(team, won, strategy) {
     const ds = []
     for (const p of team) {
         const d = {}
@@ -129,33 +271,30 @@ function toTeamData(team, won) {
             d[`move${i + 1}`] = moves[i]
         }
         d["won"] = won
+        d["strategy"] = strategy
         ds.push(d)
     }
     return ds
 }
 
-
+const LOG = false
 async function doBattles(n) {
     let ds = []
     for (let i = 0; i < n; i++) {
-        console.log(`@#${i + 1}/${n}`)
-        const b = await doBattle()
-        console.log(`@##${i + 1}/${n}`)
+        console.log(`BATTLE: #${i + 1}/${n}`)
+        const b = await doBattle(LOG)
         if (!b) {
             continue
         }
-        const [winnerTeam, loserTeam] = b;
-        const d = [...toTeamData(winnerTeam, true), ...toTeamData(loserTeam, false)]
+        const [winnerTeam, loserTeam, winnerStrategyName, loserStrategyName] = b;
+        const d = [...toTeamData(winnerTeam, true, winnerStrategyName), ...toTeamData(loserTeam, false, loserStrategyName)]
         ds = ds.concat(d)
     }
     return ds
 }
 
 
-
-
-
-doBattles(2000).then(r => {
+doBattles(500).then(r => {
     const content = convertToCSV(r)
     try {
         fs.writeFileSync('./data.csv', content);
