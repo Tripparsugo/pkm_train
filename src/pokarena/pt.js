5d4bc6f3b188d04a348570d33d979b70041c3a93
tripparsugo <lagraf@usi.ch>
2023-03-02 17:01:34 +0100

    fix tsconfig

--- src/pokarena/pt.js
+++
@@ -1,8 +0,0 @@
-import { PokemonSet } from "pokemon-showdown/.sim-dist/teams";
-// type MoveType = "attack" | "swap"
-var MoveType;
-(function (MoveType) {
-    MoveType[MoveType["ATTACK"] = 0] = "ATTACK";
-    MoveType[MoveType["SWAP"] = 1] = "SWAP";
-})(MoveType || (MoveType = {}));
-export { MoveType, PokemonSet };
